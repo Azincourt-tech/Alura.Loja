2843e5b8a98165c66870f96c345d4f6363059063
Willian da Silva Almeida <williandasilvaalmeida3@gmail.com>
2022-01-18 17:18:13 -0300

    Iniciando utilização do Migrations

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -10,67 +10,89 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-
+            #region "Funções para usar no sql"
             //GravarUsandoEntity();
             //RecuperarProdutos();
             //ExcluirProdutos();
             //RecuperarProdutos();
-            AtualizaProdutos();
+            //AtualizaProdutos();
+            #endregion
+
+
+
+
+
 
         }
 
-        private static void AtualizaProdutos()
-        {
-            GravarUsandoEntity();
-            RecuperarProdutos();
 
-            using (var repo = new ProdutoDAOEntity())
-            {
-                Produto primeiro = repo.Produtos().First();
-                primeiro.Nome = "Cassino Royale";
-                repo.Atualizar(primeiro);
-            }
 
-            RecuperarProdutos();
-        }
 
-        private static void ExcluirProdutos()
-        {
-            using (var repo = new ProdutoDAOEntity())
-            {
-                IList<Produto> produtos = repo.Produtos();
-                foreach (var item in produtos)
-                {
-                    repo.Remover(item);
-                    
-                }
-            }
-        }
 
-        private static void RecuperarProdutos()
-        {
-            using (var repo = new ProdutoDAOEntity())
-            {
-                IList<Produto> produtos = repo.Produtos();
-                Console.WriteLine("Foram encontrados {0} produtos", produtos.Count);
-                foreach (var item in produtos)
-                {
-                    Console.WriteLine(item.Nome);
-                }
-            }
-        }
 
-        private static void GravarUsandoEntity()
-        {
-            Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
-            p.Categoria = "Livros";
-            p.Preco = 19.89;
 
-            using (var contexto = new ProdutoDAOEntity())
-            {
-              contexto.Adicionar(p);
-            }
-        }
+
+
+
+
+
+        #region "Metodos do sql"
+        //private static void AtualizaProdutos()
+        //{
+        //    GravarUsandoEntity();
+        //    RecuperarProdutos();
+
+        //    using (var repo = new ProdutoDAOEntity())
+        //    {
+        //        Produto primeiro = repo.Produtos().First();
+        //        primeiro.Nome = "Cassino Royale";
+        //        repo.Atualizar(primeiro);
+        //    }
+
+        //    RecuperarProdutos();
+        //}
+
+
+        //private static void ExcluirProdutos()
+        //{
+        //    using (var repo = new ProdutoDAOEntity())
+        //    {
+        //        IList<Produto> produtos = repo.Produtos();
+        //        foreach (var item in produtos)
+        //        {
+        //            repo.Remover(item);
+
+        //        }
+        //    }
+        //}
+
+        //private static void RecuperarProdutos()
+        //{
+        //    using (var repo = new ProdutoDAOEntity())
+        //    {
+        //        IList<Produto> produtos = repo.Produtos();
+        //        Console.WriteLine("Foram encontrados {0} produtos", produtos.Count);
+        //        foreach (var item in produtos)
+        //        {
+        //            Console.WriteLine(item.Nome);
+        //        }
+        //    }
+        //}
+
+        //private static void GravarUsandoEntity()
+        //{
+        //    Produto p = new Produto();
+        //    p.Nome = "Harry Potter e a Ordem da Fênix";
+        //    p.Categoria = "Livros";
+        //    p.Preco = 19.89;
+
+        //    using (var contexto = new ProdutoDAOEntity())
+        //    {
+        //      contexto.Adicionar(p);
+        //    }
+        //}
+
+        #endregion
+
     }
 }
